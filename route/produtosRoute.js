5ad70daa6ee5d215c778cb302fc783b6ffd85a91
Camila Cristina <criscamila09@gmail.com>
2022-09-15 22:41:47 -0300

    post router pronta

--- route/produtosRoute.js
+++ route/produtosRoute.js
@@ -1,7 +1,22 @@
 const express = require('express')
+const { ColumnSet } = require('pg-promise')
 const router = express.Router()
+const { v4: uuidv4 } = require('uuid')
 
+const shoppingCar = []
 
+router.post('/carrinhodecompras', (req, res) => {
+  const { name, price, quantity, size } = req.body
 
+  shoppingCar.push({
+    id: uuidv4(),
+    name,
+    price,
+    quantity,
+    size
+  })
+
+  return res.status(201).send('Produto adicionado ao carrinho!')
+})
 
 module.exports = router
