ce04813e035d42d4160bc039e2e817a6c6351665
Camila <criscamila09@gmail.com>
2022-09-20 12:58:16 -0300

    fix get route

--- route/produtosRoute.js
+++ route/produtosRoute.js
@@ -6,11 +6,12 @@ const { v4: uuidv4 } = require("uuid");
 const shoppingCar = [];
 
 router.post("/carrinhodecompras", (req, res) => {
-  const { name, price, quantity, size } = req.body;
+  const { name, code, price, quantity, size } = req.body;
 
   shoppingCar.push({
     id: uuidv4(),
     name,
+    code,
     price,
     quantity,
     size,
@@ -19,15 +20,15 @@ router.post("/carrinhodecompras", (req, res) => {
   return res.status(201).send("Produto adicionado ao carrinho!");
 });
 
-router.get("/carrinhocompras", (req, res) => {
-  return shoppingCar;
+router.get("/carrinhodecompras", (req, res) => {
+  res.json(shoppingCar);
 });
 
-router.get("/carrinhocompras/:id", (req, res) => {
-  const id = req.params.id;
-  return shoppingCar.find((el) => el.id === id);
+router.get("/carrinhodecompras/:code", (req, res) => {
+  const code = req.params.code;
+  const findProduct = shoppingCar.find((el) => el.code === code);
+
+  res.json(findProduct);
 });
-
-
 
 module.exports = router;
